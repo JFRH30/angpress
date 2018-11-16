fb48dbef8f48c7afe9ce295fffd04e9ba24d25a3
Alaude30 <kuramamode11@gmail.com>
2018-11-16 19:07:53 +0800

    Changes : remove component from pipe module (note : use --skip-import)

--- src/app/shared/pipes.module.ts
+++ src/app/shared/pipes.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { EscapeHtmlPipe } from '../pipes/escape-html.pipe';
-import { LoaderComponent } from './components/loader/loader.component';
-import { FloatUpComponent } from './components/float-up/float-up.component';
 
 @NgModule({
-  declarations: [EscapeHtmlPipe, LoaderComponent, FloatUpComponent],
+  declarations: [EscapeHtmlPipe],
   exports: [EscapeHtmlPipe],
 })
 export class PipesModule {}
